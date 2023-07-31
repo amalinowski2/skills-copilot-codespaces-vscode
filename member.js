e66d708e03c52339046e4025afdbf97691c8b450
amalinowski2 <132664256+amalinowski2@users.noreply.github.com>
2023-07-31 12:09:44 +0000

    "Copilot second commit"

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP'];
+    var member = {
+        name: 'Code Mafia',
+        old: 3,
+        born: '2015/11/15',
+        skills: skills
+    };
+    console.log(member.skills[1]);
+}
